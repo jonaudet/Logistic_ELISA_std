fa471afe00712fd5baaf0f0809a582c0fa7aac83
Jonathan Audet <jonaudet@hotmail.com>
2017-05-16 17:52:20 -0500

    Attempt at MSD platform cytokine elisa

---
+++ MSD test.R
@@ -0,0 +1,118 @@
+if(!require(checkpoint)){
+  install.packages("checkpoint")
+  library(checkpoint)
+}
+checkpoint("2017-05-13")
+if(!(require(readr) & require(ggplot2) & require(tidyr) & require(plyr) & require(dplyr) & require(rstan) & require(rstudioapi) & require(codetools) & require(readxl))){
+  install.packages(c("rstan", "ggplot2", "tidyr", "dplyr", "plyr", "readr", "rstudioapi", "codetools", "readxl"))
+
+  library(rstan)
+  library(readr)
+  library(ggplot2)
+  library(plyr)
+  library(tidyr)
+  library(dplyr)
+  library(rstudioapi)
+  library(codetools)
+  library(readxl)
+}
+rstan_options(auto_write = TRUE)
+options(mc.cores = parallel::detectCores())
+
+HDI <- function(Values, Interval = 0.95){
+  Values <- Values[!is.na(Values)]
+  intSize <- as.integer(length(Values) * Interval)
+  startMax <- as.integer(length(Values) - (length(Values) * Interval))
+  ordered <- Values[sort.list(Values)]
+  low <- 1
+  diff <- Inf
+  for(i in 1:startMax)
+    if(ordered[i + intSize] - ordered[i] < diff){
+      low <- i
+      diff <- ordered[i + intSize] - ordered[i]
+    }
+  return(data.frame(LowerHDI = ordered[low], HigherHDI = ordered[low + intSize]))
+}
+
+unkn <- read_excel("D:/Cytokine16May.xlsx", sheet = 2) %>%
+  mutate(uID = as.numeric(factor(Sample)),
+         aID = as.numeric(factor(Assay))) %>%
+  arrange(uID, Dilution) %>%
+  mutate(Std = Sample %in% paste("Std", 1:8, sep = ""),
+         Dilution = ifelse(is.na(Dilution), Concentration, Dilution))
+
+ser_dilutions <- unkn %>%
+  group_by(Assay) %>%
+  mutate(dID = as.numeric(factor(paste(Sample, Dilution, sep = "_")))) %>%
+  distinct(dID, Dilution) %>%
+  arrange(dID) %>%
+  .$Dilution
+
+unkn <- group_by(unkn, Assay) %>%
+  mutate(dID = as.numeric(factor(paste(Sample, Dilution, sep = "_")))) %>%
+  ungroup
+
+# Plot of each plate's standard and corresponding unknowns
+mutate(unkn, Conc = 100 * Dilution) %>%
+  ggplot(aes(x = Conc, y = Signal, colour = Std, group = uID)) +
+  geom_point(alpha = 0.4) +
+  #stat_summary(aes(fun.data = "mean"), geom = "line") +
+  scale_x_log10() +
+  theme_bw() +
+  facet_wrap(~Assay, ncol = 3)
+
+mutate(unkn, Conc = 4500 * Dilution) %>%
+  filter(Std == TRUE) %>%
+  ggplot(aes(x = Conc, y = Signal, colour = Assay)) +
+  geom_point(alpha = 0.4) +
+  stat_summary(aes(fun.data = "mean"), geom = "line") +
+  scale_x_log10() +
+  theme_bw()
+
+
+initial <- function(N_dil, N_plates, N_grp){
+  inits <- list(std_raw = rnorm(1, 0, 1),
+                sigma_y = abs(rnorm(1, 0, 10)),
+                Bottom = abs(rnorm(N_plates, 250, 90)),
+                Span = rnorm(N_plates, 1, 0.3),
+                log_Inflec = rnorm(N_plates, 10, 3),
+                Slope = abs(rnorm(N_plates, 1, 0.5)),
+                log_theta = runif(N_grp - 1, -5, 6),
+                sigma_x = rexp(1, 1),
+                sigma_OD = abs(rnorm(1, 0, 0.2)))
+  return(inits)
+}
+
+# Run the model
+
+mod <- stan_model("logistic_MSD_4p_1plate.stan")
+
+sep <- lapply(1:9, function(i){
+  print(i)
+  df <- filter(unkn, aID == i) %>%
+    mutate(uID = as.numeric(factor(Sample)),
+           dID = as.numeric(factor(paste(Sample, format(Dilution, scientific = F), sep = "_")))) %>%
+    arrange(dID, uID, Dilution) %>%
+    mutate(Std = Sample == "std")
+  inits <- lapply(1:4, function(x) initial(max(df$dID), 1, max(df$uID)))
+  ser_dilutions <- df %>%
+    mutate(dID = as.numeric(factor(paste(Sample, format(Dilution, scientific = F), sep = "_")))) %>%
+    distinct(dID, Dilution) %>%
+    arrange(dID) %>%
+    .$Dilution
+  s_df <- df %>%
+    distinct(dID, uID)
+  res <- sampling(mod,
+                  data = list(N = nrow(df),
+                              N_grp = max(s_df$uID),
+                              N_grp_dil = max(df$dID),
+                              dil_ID = df$dID,
+                              uID = s_df$uID,
+                              meas_Signal = df$Signal,
+                              dilution = ser_dilutions,
+                              mu_Std = 17000,
+                              sigma_std = 50),
+                  init = inits, chains = 4, sample_file = "dia",
+                  iter = 2000, warmup = 500, refresh = 50, control = list(adapt_delta = 0.99))
+  return(res)
+})
